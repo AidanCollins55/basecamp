42e87e2ebb441e8cbdaf76f057cbb95832904f25
Aidan Collins <Aidancollins55@gmail.com>
2026-05-12 14:28:17 -0600

    Basecamp website. Initial pages and flow and styling implemented.

---
+++ basecamp/Controllers/FoodDrinksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace basecamp.Controllers
+{
+    public class FoodDrinksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ basecamp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace basecamp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
